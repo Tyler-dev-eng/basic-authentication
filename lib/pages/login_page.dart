56e1d8fea2a5e7f8edf6a52070d683b17be63a74
tyleravochoc <tyler@avochoc.com>
2026-02-20 16:13:14 +0200

    UI additions

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:basic_authentication/components/my_button.dart';
 import 'package:basic_authentication/components/my_textfield.dart';
+import 'package:basic_authentication/components/square_tile.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -17,13 +18,14 @@ class LoginPage extends StatelessWidget {
       body: SafeArea(
         child: Center(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              SizedBox(height: 50),
+              SizedBox(height: 24),
 
               // logo
-              Icon(Icons.lock, size: 100),
+              Icon(Icons.lock, size: 80),
 
-              SizedBox(height: 50),
+              SizedBox(height: 24),
 
               // welcome back, you've been missed!
               Text(
@@ -31,7 +33,7 @@ class LoginPage extends StatelessWidget {
                 style: TextStyle(fontSize: 16, color: Colors.grey[700]),
               ),
 
-              SizedBox(height: 25),
+              SizedBox(height: 16),
 
               // username field
               MyTextField(
@@ -49,7 +51,7 @@ class LoginPage extends StatelessWidget {
                 obscureText: true,
               ),
 
-              SizedBox(height: 10),
+              SizedBox(height: 8),
 
               // forgot password?
               Padding(
@@ -68,16 +70,75 @@ class LoginPage extends StatelessWidget {
                 ),
               ),
 
-              SizedBox(height: 25),
+              SizedBox(height: 16),
 
               // sign in button
               MyButton(text: 'Sign In', onPressed: onSignInPressed),
 
+              SizedBox(height: 24),
+
               // or continue with
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Divider(thickness: 0.5, color: Colors.grey[400]),
+                    ),
+
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Text(
+                        'Or continue with',
+                        style: TextStyle(color: Colors.grey[700]),
+                      ),
+                    ),
+
+                    Expanded(
+                      child: Divider(thickness: 0.5, color: Colors.grey[400]),
+                    ),
+                  ],
+                ),
+              ),
+
+              SizedBox(height: 24),
 
               // google + apple sign in
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // google button
+                  SquareTile(imagePath: 'lib/images/google.png', onTap: () {}),
+
+                  SizedBox(width: 25),
+
+                  // apple button
+                  SquareTile(
+                    imagePath: 'lib/images/apple-logo.png',
+                    onTap: () {},
+                  ),
+                ],
+              ),
+
+              SizedBox(height: 20),
 
               // not a member? register now
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Not a member?'),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Register now',
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
