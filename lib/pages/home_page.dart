368beee64c7339ae9187e0301a8881987dbd9dea
tyleravochoc <tyler@avochoc.com>
2026-02-23 10:57:59 +0200

    Getting current user info

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,7 +2,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  final User user = FirebaseAuth.instance.currentUser!;
 
   void signOut() {
     FirebaseAuth.instance.signOut();
@@ -15,7 +17,14 @@ class HomePage extends StatelessWidget {
         title: Text('Home Page'),
         actions: [IconButton(onPressed: signOut, icon: Icon(Icons.logout))],
       ),
-      body: Column(children: [Text('Home Page')]),
+      body: Center(
+        child: Column(
+          children: [
+            Text('Welcome, ${user.email}'),
+            TextButton(onPressed: signOut, child: Text('Sign Out')),
+          ],
+        ),
+      ),
     );
   }
 }
