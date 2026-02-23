1ab1b1489684f01d0d39b8577bc812149ff18f17
tyleravochoc <tyler@avochoc.com>
2026-02-23 10:48:23 +0200

    Adding a sign out button and functionality

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,21 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  void signOut() {
+    FirebaseAuth.instance.signOut();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Column(children: [Text('Home Page')]));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+        actions: [IconButton(onPressed: signOut, icon: Icon(Icons.logout))],
+      ),
+      body: Column(children: [Text('Home Page')]),
+    );
   }
 }
